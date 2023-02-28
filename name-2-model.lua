97bfa684fecbbef6b477df69b8b2adb87fd0035c
Squishy <74333752+SQUISHY6094@users.noreply.github.com>
2023-02-28 10:51:31 -0800

    Switched Model Applying from Name to Discord ID

--- name-2-model.lua
+++ name-2-model.lua
@@ -35,26 +35,39 @@ end
 function mario_update(m)
     if (m.playerIndex == 0 or m.playerIndex ~= 0) and not modelToggle then return end
     if m.playerIndex == 0 then
-        if name_without_hex(gNetworkPlayers[0].name):find("Spoomples") then
+        if network_discord_id_from_local_index(0) == "461771557531025409" --Spoomples
+        or network_discord_id_from_local_index(0) == "901908732525559828" then --Nut
             gPlayerSyncTable[0].modelId = E_MODEL_SPOOMPLES
-        elseif name_without_hex(gNetworkPlayers[0].name):find("Cosmic") then
+
+        elseif network_discord_id_from_local_index(0) == "767513529036832799" then --Cosmic
             gPlayerSyncTable[0].modelId = E_MODEL_WEEDCAT
-        elseif name_without_hex(gNetworkPlayers[0].name):find("Trashcam") then
+
+        elseif network_discord_id_from_local_index(0) == "827596624590012457" then --Trashcam
             gPlayerSyncTable[0].modelId = E_MODEL_TRASHCAM
-        elseif name_without_hex(gNetworkPlayers[0].name):find("Skeltan") then
+
+        elseif network_discord_id_from_local_index(0) == "489114867215630336" then --Skeltan
             gPlayerSyncTable[0].modelId = E_MODEL_WOOPER
-        elseif name_without_hex(gNetworkPlayers[0].name):find("AgentX") then
+
+        elseif network_discord_id_from_local_index(0) == "490613035237507091" then --AgentX
             gPlayerSyncTable[0].modelId = E_MODEL_FREEMAN
-        elseif name_without_hex(gNetworkPlayers[0].name):find("Blocky") then
+
+        elseif network_discord_id_from_local_index(0) == "584329002689363968" then --Blocky
             gPlayerSyncTable[0].modelId = E_MODEL_BLOCKY
-        elseif name_without_hex(gNetworkPlayers[0].name):find("Yosho") or name_without_hex(gNetworkPlayers[0].name):find("DepressedYoshi") then
+
+        elseif network_discord_id_from_local_index(0) == "491581215782993931" --DepressedYoshi
+        or network_discord_id_from_local_index(0) == "711762825676193874" or network_discord_id_from_local_index(0) == "561647968084557825" then --Yosho (+Alt)
             gPlayerSyncTable[0].modelId = E_MODEL_YOSHI
-        elseif name_without_hex(gNetworkPlayers[0].name):find("KanHeaven") or name_without_hex(gNetworkPlayers[0].name):find("Bloxxel64Nya") then
+
+        elseif network_discord_id_from_local_index(0) == "799106550874243083" --KanHeaven
+        or network_discord_id_from_local_index(0) == "662354972171567105" then --Bloxxel64Nya
             gPlayerSyncTable[0].modelId = E_MODEL_NYA
-        elseif name_without_hex(gNetworkPlayers[0].name):find("0x2480") then
+
+        elseif network_discord_id_from_local_index(0) == "282702284608110593" then --0x2480
             gPlayerSyncTable[0].modelId = E_MODEL_CROC
-        elseif name_without_hex(gNetworkPlayers[0].name):find("Average") then
+
+        elseif network_discord_id_from_local_index(0) == "397219199375769620" then --Average
             gPlayerSyncTable[0].modelId = E_MODEL_NATSUKI
+
         else
             gPlayerSyncTable[0].modelId = nil
         end
@@ -62,10 +75,11 @@ function mario_update(m)
     
     if gPlayerSyncTable[m.playerIndex].modelId ~= nil then
         obj_set_model_extended(m.marioObj, gPlayerSyncTable[m.playerIndex].modelId)
-        if name_without_hex(gNetworkPlayers[0].name):find("Cosmic") then
-            gNetworkPlayers[m.playerIndex].overrideModelIndex = CT_WALUIGI
-        elseif name_without_hex(gNetworkPlayers[0].name):find("Spoomples") then
+        if network_discord_id_from_local_index(0) == "461771557531025409" --Spoomples
+        or network_discord_id_from_local_index(0) == "901908732525559828" then --Nut
             gNetworkPlayers[m.playerIndex].overrideModelIndex = CT_TOAD
+        elseif network_discord_id_from_local_index(0) == "767513529036832799" then --Cosmic
+            gNetworkPlayers[m.playerIndex].overrideModelIndex = CT_WALUIGI
         end
     end
 end
