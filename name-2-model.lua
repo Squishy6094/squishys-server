3b03cee90733b891688be6b0ef4e938117d10e14
Plusiaa <dgutierrezgamer@gmail.com>
2023-07-02 23:35:52 -0400

    Update name-2-model.lua

--- name-2-model.lua
+++ name-2-model.lua
@@ -515,8 +515,8 @@ modelTable = {
             forcePlayer = CT_MARIO
         },
         [4] = {
-            model = smlua_model_util_get_id("boshi_geo"),
-            modelName = "Boshi",
+            model = smlua_model_util_get_id("kirby_geo"),
+            modelName = "Kirby",
             forcePlayer = CT_TOAD
         }
     },
