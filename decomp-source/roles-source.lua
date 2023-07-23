814cd0c01eff44e12635141b9277698daa76161f
Squishy <74333752+SQUISHY6094@users.noreply.github.com>
2023-07-23 06:37:34 -0700

    (Hopefully) Fixed randoms being mis-roled

--- decomp-source/roles-source.lua
+++ decomp-source/roles-source.lua
@@ -1,46 +1,50 @@
 local ID = tonumber(network_discord_id_from_local_index(0))
 
+-- 1 = Creator
+-- 2 = Developer
+-- 3 = Verified Host
+-- 4 = Contributer
+-- 5 = Moderator
+
+-- 0 = Default User
+-- -1 = Unverified Host
+
+roleIDtable = {
+    --Creator
+    [678794043018182675] = "1", --Squishy
+
+    --Developer
+    [635629441678180362] = "2", --Plussle
+
+    --Gm_Boo3volved (Verified Host)
+    [401406794649436161] = "3",  --Uoker
+    [673582558507827221] = '3',  --Elby
+    [664638362484867121] = "3",  --Kitkat
+    [397847847283720193] = "3",  --Koffee
+    --Skedar (Verified Host)
+    [490613035237507091] = "3 4",--Agent X (Tons of help overall with coding)
+    [376304957168812032] = "3",  --eros71
+    [282702284608110593] = "3",  --0x
+    [767513529036832799] = "3",  --Cosmic
+    [542676894244536350] = "3",  --Floralys
+    [827596624590012457] = "3", --Trashcam
+    [443963592220344320] = "3", --Charity
+    [732244024567529503] = "3", --PeachyPeach
+    --Epic Gamer Squad (Verified Host)
+    [397219199375769620] = "3", --Average
+    [817821798363955251] = "3", --Crispy
+    [1092073683377455215] ="3", --Nut
+
+    --Contributer
+    [409438020870078486] = "4", --EmilyEmmi (The entire Roles System)
+    [376426041788465173] = "4", --Sunk (A bunch of QOL mods)
+}
+
 function update_roles()
     local m = gMarioStates[0]
     local sMario = gPlayerSyncTable[0]
 
     if ID ~= 0 then
-        -- 1 = Creator
-        -- 2 = Developer
-        -- 3 = Verified Host
-        -- 4 = Contributer
-        -- 5 = Moderator
-        -- -1 = Unverified Host
-        roleIDtable = {
-            --Creator
-            [678794043018182675] = "1", --Squishy
-        
-            --Developer
-            [635629441678180362] = "2", --Plussle
-        
-            --Gm_Boo3volved (Verified Host)
-            [401406794649436161] = "3",  --Uoker
-            [673582558507827221] = '3',  --Elby
-            [664638362484867121] = "3",  --Kitkat
-            [397847847283720193] = "3",  --Koffee
-            --Skedar (Verified Host)
-            [490613035237507091] = "3 5",--Agent X (Tons of help overall with coding)
-            [376304957168812032] = "3",  --eros71
-            [282702284608110593] = "3",  --0x
-            [767513529036832799] = "3",  --Cosmic
-            [542676894244536350] = "3",  --Floralys
-            [827596624590012457] = "3", --Trashcam
-            [443963592220344320] = "3", --Charity
-            [732244024567529503] = "3", --PeachyPeach
-            --Epic Gamer Squad (Verified Host)
-            [397219199375769620] = "3", --Average
-            [817821798363955251] = "3", --Crispy
-            [1092073683377455215] ="3", --Nut
-
-            --Contributer
-            [409438020870078486] = "4", --EmilyEmmi (The entire Roles System)
-            [376426041788465173] = "4", --Sunk (A bunch of QOL mods)
-        }
         if roleIDtable[ID] ~= nil then
             sMario.role = roleIDtable[ID]
         else
@@ -55,6 +59,7 @@ function update_roles()
             sMario.ismod = true
         end
     else
+        sMario.role = "0"
         if network_is_server() then
             sMario.role = "-1"
         end
@@ -71,20 +76,20 @@ function on_chat_message(m, msg)
         rolestring = ""
         local args = split(sMario.role)
         for i = 1, 6 do
-            if args[i] == "1" then
+            if tonumber(args[i]) == 1 then
                 rolestring = rolestring.." \\#00aa00\\[Creator]"
             end 
-            if args[i] == "2" then
+            if tonumber(args[i]) == 2 then
                 rolestring = rolestring.." \\#FF2400\\[Developer]"
             end
-            if args[i] == "3" then
+            if tonumber(args[i]) == 3 then
                 if sMario.ishost then
                     rolestring = rolestring.." \\#7FFFD4\\[Verified Host]"
                 else
                     rolestring = rolestring.." \\#f23064\\[Bestie]"
                 end
             end 
-            if args[i] == "4" then
+            if tonumber(args[i]) == 4 then
                 rolestring = rolestring.." \\#0568e3\\[Contributor]"
             end 
         end
@@ -94,7 +99,7 @@ function on_chat_message(m, msg)
         if sMario.role == "-1" then
             rolestring = " \\#ff0000\\[Unverified Host]"
         end 
-        if rolestring ~= "" then
+        if rolestring ~= "0" then
             djui_chat_message_create(name..""..rolestring..": \\#dcdcdc\\"..msg)
         end
 
@@ -105,6 +110,8 @@ function on_chat_message(m, msg)
             play_sound(SOUND_MENU_MESSAGE_APPEAR, myM.marioObj.header.gfx.cameraToObject)
         end
         return false
+    else
+        sMario.role = "0"
     end
 end
 hook_event(HOOK_ON_SYNC_VALID, update_roles)
