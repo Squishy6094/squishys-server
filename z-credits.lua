64c29d5d662466bb9f1017f421ab0b50b9df268e
Squishy <74333752+SQUISHY6094@users.noreply.github.com>
2023-09-15 19:59:07 -0700

    More progress, but now I am eepy

--- z-credits.lua
+++ z-credits.lua
@@ -7,48 +7,42 @@ local creditsTable = {
     {
         sectionName = "Developers",
         sectionColor = {r = 255, g = 36, b = 0},
-        sectionColumns = 1,
+        sectionColumns = 2,
+        sectionPos = 30,
         {
             name = "Squishy",
-            handle = "@6094Squishy - Twitter",
+            color = {r = 0, g = 150, b = 0},
+            handle = "squishy6094",
             creditFor = "Lead Developer"
         },
         {
             name = "Floralys",
-            handle = "@xfloralys - Twitter",
+            handle = "xfloralys",
             creditFor = "Secondary Developer"
         },
         {
             name = "Plusle",
-            handle = "staticgnawer - Discord",
+            handle = "staticgnawer",
         },
         {
             name = "Crispyman",
-            handle = "@Crispus_Manus - Twitter",
+            handle = "crispyman.",
         },
     },
-    --[[
     {
         sectionName = "Support / Contribution",
         sectionColumns = 4,
+        sectionPos = 120,
         {
             name = "Agent X",
-            creditFor = "Tons of coding support"
         },
         {
             name = "EmilyEmmi",
-            creditFor = "Roles System"
         },
         {
-            name = "Plusle",
-            handle = "staticgnawer / Discord",
-        },
-        {
-            name = "Crispyman",
-            handle = "@Crispus_Manus / Twitter",
+            name = "Sunk",
         },
     }
-    --]]
 }
 
 local djui_hud_set_resolution = djui_hud_set_resolution
@@ -70,14 +64,27 @@ function render_credits()
     if creditsOpen then
         djui_hud_set_resolution(RESOLUTION_N64)
         djui_hud_set_font(FONT_NORMAL)
-        djui_hud_set_color(150, 150, 150, 255)
+        djui_hud_set_color(0, 0, 0, 255)
         djui_hud_render_rect(0, 0, djui_hud_get_screen_width() + 5, djui_hud_get_screen_height() + 5)
         for i = 1, #creditsTable do
-            if creditsTable[i].sectionColor == nil then creditsTable[i].sectionColor = {r = 0, g = 88, b = 0} end
+            if creditsTable[i].sectionColor == nil then creditsTable[i].sectionColor = {r = 0, g = 150, b = 0} end
             djui_hud_set_color(creditsTable[i].sectionColor.r, creditsTable[i].sectionColor.g, creditsTable[i].sectionColor.b, 255)
-            djui_hud_print_text(creditsTable[i].sectionName, djui_hud_get_screen_width() * 0.5 - djui_hud_measure_text(creditsTable[i].sectionName) * 0.5, 30 - creditsScroll, 1)
+            djui_hud_print_text(creditsTable[i].sectionName, djui_hud_get_screen_width() * 0.5 - djui_hud_measure_text(creditsTable[i].sectionName) * 0.5, creditsTable[i].sectionPos + creditsScroll, 1)
             for k = 1, #creditsTable[i] do
-                djui_hud_print_text(creditsTable[i][k].name, (djui_hud_get_screen_width() * 0.5 - creditsTable[i].sectionColumns/2) + (k / creditsTable[i].sectionColumns) * 50 - djui_hud_measure_text(creditsTable[i][k].name) * 0.25, 30 + math.floor(creditsTable[i].sectionColumns/k) * 10 - creditsScroll, 0.5)
+                if creditsTable[i][k].color == nil then creditsTable[i][k].color = creditsTable[i].sectionColor end
+                djui_hud_set_color(creditsTable[i][k].color.r, creditsTable[i][k].color.g, creditsTable[i][k].color.b, 255)
+
+                local scale = 0.5
+                local x = (djui_hud_get_screen_width() * 0.5) - (djui_hud_measure_text(creditsTable[i][k].name)*(scale*0.5)) + (k%creditsTable[i].sectionColumns) * 55 - (creditsTable[i].sectionColumns - 1)*27.5
+                local y = creditsTable[i].sectionPos + 15 + creditsScroll + math.ceil(k/creditsTable[i].sectionColumns)*25
+
+                djui_hud_print_text(creditsTable[i][k].name, x, y, scale)
+                if creditsTable[i][k].handle ~= nil then
+                    local scale = 0.25
+                    local x = (djui_hud_get_screen_width() * 0.5) - (djui_hud_measure_text(creditsTable[i][k].handle)*(scale*0.5)) + (k%creditsTable[i].sectionColumns) * 55 - (creditsTable[i].sectionColumns - 1)*27.5
+                    y = y + 15
+                    djui_hud_print_text(creditsTable[i][k].handle, x, y, scale)
+                end
             end
         end
     end
@@ -88,6 +95,11 @@ function inputs(m)
         if m.controller.buttonDown & B_BUTTON ~= 0 then
             creditsOpen = false
         end
+
+        if math.abs(m.controller.stickY) >= 5 then
+            creditsScroll = creditsScroll + m.controller.stickY*0.05
+        end
+
         nullify_inputs(m)
     end
 end
