a111c05f24eba3854dbfdcba5392e4213eec907a
Squishy <74333752+SQUISHY6094@users.noreply.github.com>
2023-09-28 07:58:02 -0700

    Updated nullify_inputs(), I'm so stupid lmao

--- a-functions.lua
+++ a-functions.lua
@@ -51,19 +51,13 @@ end
 
 ---@param m MarioState
 function nullify_inputs(m)
-    m.controller.rawStickY = 0
-    m.controller.rawStickX = 0
-    m.controller.stickX = 0
-    m.controller.stickY = 0
-    m.controller.stickMag = 0
-    m.controller.buttonPressed = m.controller.buttonPressed & ~R_TRIG
-    m.controller.buttonDown = m.controller.buttonDown & ~R_TRIG
-    m.controller.buttonPressed = m.controller.buttonPressed & ~A_BUTTON
-    m.controller.buttonDown = m.controller.buttonDown & ~A_BUTTON
-    m.controller.buttonPressed = m.controller.buttonPressed & ~B_BUTTON
-    m.controller.buttonDown = m.controller.buttonDown & ~B_BUTTON
-    m.controller.buttonPressed = m.controller.buttonPressed & ~START_BUTTON
-    m.controller.buttonDown = m.controller.buttonDown & ~START_BUTTON
+     m.controller.rawStickY = 0 
+     m.controller.rawStickX = 0 
+     m.controller.stickX = 0 
+     m.controller.stickY = 0 
+     m.controller.stickMag = 0 
+     m.controller.buttonPressed = 0 
+     m.controller.buttonDown = 0
 end
 
 ---@param compare_object Object
